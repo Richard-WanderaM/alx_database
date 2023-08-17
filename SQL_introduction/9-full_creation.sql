987e2537f0adacca8d933519427ffaca9381f504
Richard-WanderaM <richardwandera2023@gmail.com>
2023-08-17 22:09:42 +0300

    script that creates a table second_table in the database

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -5,4 +5,9 @@ CREATE TABLE IF NOT EXISTS second_table (
     score INT
 );
 
+-- Insert rows into the table second_table
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
 
